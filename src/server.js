24098c7ae4c0bda5dc8576838abf2efae1e312c2
acelsie14 <aloziechineduelsie@gmail.com>
2026-09-03 17:24:08 +0100

    chnaged url

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_LOCAL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Databse connected');
   } catch (error) {
     console.error('Datbase connection error: ', error.message);
